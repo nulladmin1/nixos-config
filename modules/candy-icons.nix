08fac6fdde9d9da7138a0bd3a75007331adb0f41
nulladmin1 <shrey.deogade@protonmail.com>
2024-01-26 16:08:40 -0500

    Added Candy Icons derivation

---
+++ modules/candy-icons.nix
@@ -0,0 +1,18 @@
+# For Home-Manager
+{ pkgs }:
+
+pkgs.stdenv.mkDerivation {
+  name = "candy-icons";
+
+  src = pkgs.fetchurl {
+    url = "https://github.com/EliverLara/candy-icons/archive/refs/heads/master.zip";
+    sha256 = "1b7jh8rcpa35v3i4y1ddfhv29pqw6riishrplyhjyg3rknbzspgq";
+  };
+
+  dontUnpack = true;
+
+  installPhase = ''
+    mkdir -p $out
+    ${pkgs.unzip}/bin/unzip $src -d $out/
+  '';
+}
