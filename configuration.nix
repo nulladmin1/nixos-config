728c97303ac7bf62ff6a0d04cba610c29a2d2859
nulladmin1 <shrey.deogade@protonmail.com>
2024-03-06 21:21:51 -0500

    Added wireshark

--- configuration.nix
+++ configuration.nix
@@ -222,6 +222,9 @@
     gamescopeSession.enable = true;
   };
 
+  # Enable WireShark
+  programs.wireshark.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
