173b45f09ef610f3da278213a58e20117affc706
nulladmin1 <shrey.deogade@protonmail.com>
2024-04-18 20:15:00 -0400

    Removed OVMF

--- system/modules/virtualization.nix
+++ system/modules/virtualization.nix
@@ -7,12 +7,6 @@
       qemu = {
         package = pkgs.qemu_kvm;
         swtpm.enable = true;
-        ovmf = {
-          enable = true;
-          packages = [(pkgs.OVMF.override {
-            secureBoot = true;
-            tpmSupport = true;
-          }).fd];
         };
       };
     };
