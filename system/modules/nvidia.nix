ef0858108d39497b16c8c3cfd58088a1157bdf16
nulladmin1 <shrey.deogade@protonmail.com>
2024-09-14 21:36:44 -0400

    Added nvidia config

---
+++ system/modules/nvidia.nix
@@ -0,0 +1,32 @@
+{ config, lib, ... }:
+
+{
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    open = false;
+
+    nvidiaSettings = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    powerManagement = {
+      enable = false;
+      finegrained = false;
+    };
+
+    prime = {
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:01:0:0";
+    };
+
+    sync.enable = true;
+  };
+}
