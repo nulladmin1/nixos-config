df6096e490213a6eaaf8c4ef409e41fc75524e64
nulladmin1 <shrey.deogade@protonmail.com>
2024-09-21 14:45:58 -0400

    Removed catppucin setting; already enabled globally

--- home-manager/modules/starship.nix
+++ home-manager/modules/starship.nix
@@ -2,24 +2,12 @@
 
 {
   # Starship
-  programs.starship = let
-    flavour = "macchiato";
-  in {
+  programs.starship = {
     enable = true;
     settings =
       {
         # Other config here
         #        format = "$all"; # Remove this line to disable the default prompt format
-        palette = "catppuccin_${flavour}";
-      }
-      // builtins.fromTOML (builtins.readFile
-        (pkgs.fetchFromGitHub
-          {
-            owner = "catppuccin";
-            repo = "starship";
-            rev = "5629d2356f62a9f2f8efad3ff37476c19969bd4f"; # Replace with the latest commit hash
-            sha256 = "sha256-nsRuxQFKbQkyEI4TXgvAjcroVdG+heKX5Pauq/4Ota0=";
-          }
-          + /palettes/${flavour}.toml));
+      };
   };
 }
