af66fa3af6b6ca0512583779939290994e2a6960
nulladmin1 <shrey.deogade@protonmail.com>
2024-09-21 12:41:45 -0400

    Added background for sddm catppuccin

--- system/modules/system.nix
+++ system/modules/system.nix
@@ -73,8 +73,18 @@
     displayManager = {
       sddm = {
         enable = true;
-#        theme = "${import ./sddm-theme.nix {inherit pkgs;}}";
         wayland.enable = true;
+        catppuccin = {
+          background = let
+            catppuccin_wallpapers = pkgs.fetchFromGitHub {
+              owner = "Gingeh";
+              repo = "wallpapers";
+              rev = "2530dba028589bda0ef6743d7960bd8a5b016679";
+              hash = "sha256-FiilS+t6QszTsnaIFVRRC8pQ6oTcv6qvKMf9cD5AoBQ=";
+            };
+            in "${catppuccin_wallpapers}/misc/virus.png";
+          font = "JetBrainsMono Nerd Font";
+        };
       };
     };
   };
