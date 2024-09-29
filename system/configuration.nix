404fbc522ee789fb218621f32ad20d35fd1a72af
nulladmin1 <shrey.deogade@protonmail.com>
2024-09-29 11:51:12 -0400

    Enabled extra compat path for steam

--- system/configuration.nix
+++ system/configuration.nix
@@ -61,6 +61,10 @@
     waybar
   ];
 
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/${username}/.steam/root/compatibilitytools.d";
+  };
+
   # Shells
   environment.shells = with pkgs; [bash zsh fish];
   users.defaultUserShell = pkgs.bash;
