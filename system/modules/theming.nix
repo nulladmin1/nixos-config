8b8bc3f25e575272484e18599de61467cc637313
nulladmin1 <shrey.deogade@protonmail.com>
2024-10-08 22:24:19 -0400

    Moved SDDM theming to theming.nix

---
+++ system/modules/theming.nix
@@ -0,0 +1,15 @@
+{ pkgs, lib, wallpaper, ... }:
+{
+  services.displayManager.sddm.catppuccin = {
+          background = let
+            catppuccin_wallpapers = pkgs.fetchFromGitHub {
+              owner = "Gingeh";
+              repo = "wallpapers";
+              rev = "2530dba028589bda0ef6743d7960bd8a5b016679";
+              hash = "sha256-FiilS+t6QszTsnaIFVRRC8pQ6oTcv6qvKMf9cD5AoBQ=";
+            };
+          in "${catppuccin_wallpapers}/misc/virus.png";
+          font = "JetBrainsMono Nerd Font";
+        };
+
+}
