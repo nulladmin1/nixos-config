1f111470877c732d2bf9f1cb6023161eaf2fb295
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-14 21:04:42 -0500

    Remove obsolete options for decoration::shadow

--- home-manager/modules/hyprland.nix
+++ home-manager/modules/hyprland.nix
@@ -77,10 +77,8 @@
         active_opacity = 1.0;
         inactive_opacity = 1.0;
 
-        drop_shadow = true;
-        shadow_range = 4;
-        shadow_render_power = 3;
-        "col.shadow" = "rgba(1a1a1aee)";
+        shadow.range = 4;
+        shadow.render_power = 3;
       
     
         blur = {
