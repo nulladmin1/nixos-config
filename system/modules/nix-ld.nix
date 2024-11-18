7e6a40cf7fdd15cf2bcb7474a1201199f9c0fba8
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-18 02:03:17 -0500

    Use steam-run libraries for nix-ld

--- system/modules/nix-ld.nix
+++ system/modules/nix-ld.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
-  programs.nix-ld = { 
-    enable = true; 
-#    libraries = pkgs.steam-run.fhsenv.args.multiPkgs pkgs; 
+  programs.nix-ld = {
+    enable = true;
+    libraries = pkgs.steam-run.args.multiPkgs pkgs;
   };
 }
