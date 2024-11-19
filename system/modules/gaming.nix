291a05e48aa65aaa2673dfff611241c62f8eb96d
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-18 23:13:49 -0500

    Add Steam platformOptimizations from nix-gaming

--- system/modules/gaming.nix
+++ system/modules/gaming.nix
@@ -1,10 +1,13 @@
-{ ... }:
-{
+{...}: {
   # Steam
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
-  }; 
+
+    # https://github.com/fufexan/nix-gaming#platform-optimizations
+    # A bunch of optimizations for Steam from SteamOS
+    platformOptimizations.enable = true;
+  };
 
   # gamemoderun
   programs.gamemode.enable = true;
