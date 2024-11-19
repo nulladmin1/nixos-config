dba4acf5794ca88e42cf277c73cb2ad45184030b
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-19 00:11:14 -0500

    Add hypridle stuff

--- home/modules/desktop/hyprland.nix
+++ home/modules/desktop/hyprland.nix
@@ -24,6 +24,8 @@
     enable = true;
   };
 
+  services.swayosd.enable = true;
+
   services.hyprpaper = {
     enable = true;
     settings = {
@@ -32,7 +34,28 @@
     };
   };
 
-  services.swayosd.enable = true;
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
+
+      listener = [
+        {
+          timeout = 180;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 300;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+      ];
+    };
+  };
 
   programs.hyprlock = {
     enable = true;
