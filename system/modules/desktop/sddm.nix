c18c273c6743c8d03d724d64da46fb1962eebe2b
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-19 23:16:50 -0500

    Refactor SDDM theme into sddm.nix

--- system/modules/desktop/sddm.nix
+++ system/modules/desktop/sddm.nix
@@ -1,10 +1,18 @@
-{...}: {
+{
+  wallpaper,
+  font,
+  ...
+}: {
   # Enable SDDM
   services = {
     displayManager = {
       sddm = {
         enable = true;
         wayland.enable = true;
+        catppuccin = {
+          background = wallpaper;
+          inherit font;
+        };
       };
     };
   };
--- system/modules/theming.nix
+++ system/modules/theming.nix
@@ -1,15 +1,4 @@
-{
-  pkgs,
-  lib,
-  wallpaper,
-  font,
-  ...
-}: {
-  services.displayManager.sddm.catppuccin = {
-    background = wallpaper;
-    inherit font;
-  };
-
+{...}: {
   catppuccin = {
     enable = true;
     flavor = "mocha";
