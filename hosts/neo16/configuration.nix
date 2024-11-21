c5e57cd8dcd5b3b266e8297676f9b228d43083ef
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-21 00:14:26 -0500

    Refactor direnv into home-manager

--- hosts/neo16/configuration.nix
+++ hosts/neo16/configuration.nix
@@ -58,13 +58,6 @@ in {
     source ${pkgs.nix-index}/etc/profile.d/command-not-found.sh
   '';
 
-  programs.direnv = {
-    enable = true;
-    enableBashIntegration = true;
-    loadInNixShell = true;
-    nix-direnv.enable = true;
-  };
-
   fonts.packages = with pkgs; [
     nerdfonts
     fira-code
@@ -73,24 +66,10 @@ in {
   ];
   fonts.fontDir.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
   nix.settings = {
     experimental-features = ["nix-command" "flakes"];
     auto-optimise-store = true;
   };
-
-  boot.tmp.cleanOnBoot = true;
 }
