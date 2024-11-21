64143a8f4f3bd161925d73111e84b50e0ee32f4c
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-20 21:33:14 -0500

    Rename var.nix to opts.nix

--- hosts/neo16/opts.nix
+++ hosts/neo16/opts.nix
@@ -4,18 +4,18 @@
   system,
   ...
 }: {
-  config.var = let
-    username = "shreyd";
-  in {
+  imports = [../../shared/var-options.nix];
+
+  config.var = {
     hostname = "shrey-neo16-nixos";
     name = "Shrey Deogade";
     email = "shrey.deogade@protonmail.com";
-    inherit username;
+    username = "shreyd";
 
     git_username = "nulladmin1";
     git_email = "shrey.deogade@protonmail.com";
 
-    flake = "/home/${username}/nixos-config/";
+    flake = "/home/shreyd/nixos-config/";
 
     locale = "en_US.UTF-8";
 
