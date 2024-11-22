f22ffa56b4f8d5e0168cd6d1aedfe1d319ff0678
nulladmin1 <shrey.deogade@protonmail.com>
2024-11-22 15:51:18 -0500

    Use Nvidia beta drivers instead of stable

--- system/hardware/nvidia.nix
+++ system/hardware/nvidia.nix
@@ -17,7 +17,7 @@
 
     nvidiaSettings = true;
 
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
 
     powerManagement = {
       enable = false;
