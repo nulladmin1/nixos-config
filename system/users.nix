2648e6925011284a829bfa5d3c6ffceda98c401c
nulladmin1 <shrey.deogade@protonmail.com>
2024-12-15 20:48:05 -0500

    Disable sops-nix storage of nix secrets for now - got locked out of my PC

--- system/users.nix
+++ system/users.nix
@@ -6,14 +6,14 @@
   inherit (config.var) username;
   inherit (config.var) name;
 in {
-  sops.secrets.shreyd-password.neededForUsers = true;
-  users.mutableUsers = false;
+#  sops.secrets.shreyd-password.neededForUsers = true;
+#  users.mutableUsers = false;
 
   users.users.${username} = {
     isNormalUser = true;
     description = name;
     extraGroups = ["networkmanager" "wheel" "audio" "adbusers" "lp"];
-    hashedPasswordFile = config.sops.secrets.shreyd-password.path;
+#    hashedPasswordFile = config.sops.secrets.shreyd-password.path;
 
     openssh.authorizedKeys.keys = [
       (builtins.readFile ../shared/keys/id_ed25519.pub)
