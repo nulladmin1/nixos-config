40f3dd3625d157069b0e7166df6f91d570294f88
nulladmin1 <shrey.deogade@protonmail.com>
2024-12-18 19:05:09 -0500

    Add script to automatically update flake and commit

---
+++ justfile
@@ -0,0 +1,11 @@
+alias up := update
+alias upc := update-with-commits
+
+default:
+    @just --list
+
+update:
+    nix flake update
+
+update-with-commits:
+    scripts/flake.sh
--- scripts/flake.sh
+++ scripts/flake.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-input_text="
+flake_changes=$(nix flake update 2>&1)
+commit_message=""
 
-"
+while IFS= read -r line; do 
+  if [[ "$line" =~ Updated\ input\ \'([^\']+)\' ]]; then 
+      name="${BASH_REMATCH[1]}" 
+  fi 
+  if [[ "$line" =~ \→.*\(([0-9\-]+)\) ]]; then 
+      latest_date="${BASH_REMATCH[1]}" 
+      commit_message+="$name: ($latest_date)"$'\n' 
+  fi 
+done <<< "$flake_changes"
 
-echo "$input_text" | while IFS= read -r line; do
-  if [[ "$line" =~ Updated\ input\ \'([^\']+)\' ]]; then
-    name="${BASH_REMATCH[1]}"
-  fi
-  if [[ "$line" =~ \→.*\(([0-9\-]+)\) ]]; then
-    latest_date="${BASH_REMATCH[1]}"
-    echo "$name: ($latest_date)"
-  fi
-done
+echo "Commit message: "
+echo "$commit_message"
+
+git add flake.lock
+git commit -m "Update flake inputs
+$commit_message"
