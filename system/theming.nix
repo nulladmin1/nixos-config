95d21707aeb7ea6bb0d8dc99def905babfe3c306
nulladmin1 <shrey.deogade@protonmail.com>
2024-12-21 22:57:42 -0500

    Import stylix theme

--- system/theming.nix
+++ system/theming.nix
@@ -1,10 +1,16 @@
 {
   pkgs,
+  lib,
   config,
+  inputs,
   ...
 }: let
   inherit (config.var) wallpaper;
 in {
+  imports = [
+    inputs.stylix.nixosModules.stylix
+  ];
+
   stylix = {
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-${config.catppuccin.flavor}.yaml";
     image = wallpaper;
