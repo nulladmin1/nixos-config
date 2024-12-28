96cc987121d06dbfa156a379829b6fb2fe0adc21
nulladmin1 <shrey.deogade@protonmail.com>
2024-12-28 13:26:56 -0500

    Remove unnecessary shell aliases

--- home/bash.nix
+++ home/bash.nix
@@ -1,6 +1,4 @@
-{config, ...}: let
-  inherit (config.var) editor;
-in {
+{...}: {
   # Bash:
   programs.bash = {
     enable = true;
@@ -11,24 +9,13 @@ in {
       "....." = "cd ../../../..";
       h = "history | fzf";
       ":q" = "exit";
-      meow = "cat";
-      vc = "virtualenv venv";
-      va = "source ./venv/bin/activate";
+      meow = "bat";
       cd = "z";
       please = "sudo ";
       g = "git";
       push = "git push";
       pull = "git pull";
       commit = "git commit -am";
-      edithome = "cd ~/nixos-config/ && ${editor} home-manager/home.nix home-manager/packages.nix";
-      editsysconf = "cd ~/nixos-config/ && ${editor} system/configuration.nix flake.nix";
-      #nvidia-offload = "export __NV_PRIME_RENDER_OFFLOAD=1
-      #    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-      #    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-      #    export __VK_LAYER_NV_optimus=NVIDIA_only
-      #    exec '$@'";
-      # showcursor = "printf '\033[?25l'";
-      # hidecursor = "printf '\033[?25h'";
     };
     bashrcExtra = ''
       nerdfetch
