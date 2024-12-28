13b94725a8f286f3d221b543866afd64592d5530
nulladmin1 <shrey.deogade@protonmail.com>
2024-12-28 15:17:14 -0500

    Refactor system-specific and hardware-configuration.nix into configuration.nix

--- hosts/neo16/configuration.nix
+++ hosts/neo16/configuration.nix
@@ -1,7 +1,9 @@
 {
   config,
   pkgs,
+  lib,
   inputs,
+  modulesPath,
   ...
 }: let
   inherit (config.var) username;
@@ -14,8 +16,11 @@ in {
     inputs.disko.nixosModules.disko
     ./disko.nix
 
-    ./hardware-configuration.nix
+    (modulesPath + "/installer/scan/not-detected.nix")
   ];
+
+  home-manager.users.${username} = import ../common/home.nix;
+
   networking = {
     hostName = "neo16";
     nameservers = ["1.1.1.1" "1.0.0.1"];
@@ -25,7 +30,36 @@ in {
     };
   };
 
-  home-manager.users.${username} = import ../common/home.nix;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  boot = {
+    initrd.availableKernelModules = ["vmd" "xhci_pci" "thunderbolt" "nvme" "usb_storage" "usbhid" "sd_mod" "rtsx_pci_sdmmc"];
+    initrd.kernelModules = [];
+    kernelModules = ["kvm-intel"];
+    extraModulePackages = [];
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    powerManagement = {
+      enable = false;
+      finegrained = false;
+    };
+    prime = {
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:01:0:0";
+      sync.enable = true;
+      offload = {
+        enable = !config.hardware.nvidia.prime.sync.enable;
+        enableOffloadCmd = !config.hardware.nvidia.prime.sync.enable;
+      };
+    };
+  };
 
   system.stateVersion = "23.11";
 }
