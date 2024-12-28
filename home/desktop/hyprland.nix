a3ba43269af5797dc25928afc1a6d672a5ead450
nulladmin1 <shrey.deogade@protonmail.com>
2024-12-27 23:00:19 -0500

    Enable QT Theming

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -165,7 +165,6 @@ in {
         "QT_AUTO_SCREEN_SCALE_FACTOR,1"
         "QT_QPA_PLATFORM,wayland;xcb"
         "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
-        "QT_QPA_PLATFORMTHEME,qt6ct"
 
         # Nvidia + Wayland
         "LIBVA_DRIVER_NAME,nvidia"
--- home/theming.nix
+++ home/theming.nix
@@ -1,5 +1,6 @@
 {
-  inputs,
+  config,
+  osConfig,
   lib,
   ...
 }: {
@@ -8,6 +9,18 @@
       enable = false;
     };
 
-    gtk.enable = false;
+  qt = let
+    osTheme = osConfig.qt.platformTheme;
+  in {
+    inherit (osConfig.qt) enable;
+    platformTheme.name =
+      if config.catppuccin.kvantum.enable
+      then "kvantum"
+      else if osTheme == "gtk2"
+      then "gtk"
+      else osTheme;
+    style = lib.mkIf config.catppuccin.kvantum.enable {
+      name = "kvantum";
+    };
   };
 }
