e005d9c74faf5f9294bb7b323b69cf1f5ce689f9
nulladmin1 <shrey.deogade@protonmail.com>
2024-12-29 10:51:34 -0500

    Add lib/ to house common lib functions

---
+++ lib/default.nix
@@ -0,0 +1,1 @@
+lib: {customLib = import ./lib.nix lib;}
---
+++ lib/lib.nix
@@ -0,0 +1,19 @@
+lib: prev: {
+  lib =
+    prev.lib
+    // {
+      customLib = {
+        capitalize = let
+          inherit (prev.lib.strings) toUpper substring;
+        in
+          str: toUpper (substring 0 1 str) + substring 1 (builtins.stringLength str) str;
+
+        prefer = pairs: let
+          result = builtins.filter (pair: pair.condition) pairs;
+        in
+          if result == []
+          then null
+          else (builtins.head result).value;
+      };
+    };
+}
