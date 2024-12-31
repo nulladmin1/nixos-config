3ddc8310dc657dcbc8b05d6eb4508647f8b2eb9c
nulladmin1 <shrey.deogade@protonmail.com>
2024-12-31 10:17:45 -0500

    Use hyprland-nixpkgs's mesa drivers

--- system/desktop/hyprland.nix
+++ system/desktop/hyprland.nix
@@ -1,9 +1,12 @@
 {
   inputs,
   pkgs,
+  lib,
+  config,
   ...
 }: let
   inherit (inputs.hyprland.packages.${pkgs.system}) hyprland xdg-desktop-portal-hyprland;
+  hyprland-nixpkgs = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.system};
 in {
   # Hyprland
   programs.hyprland = {
@@ -11,5 +14,11 @@ in {
     xwayland.enable = true;
     package = hyprland;
     portalPackage = xdg-desktop-portal-hyprland;
+  };
+
+  # Better performance in Hyprland
+  hardware.graphics = {
+    package = hyprland-nixpkgs.mesa.drivers;
+    package32 = hyprland-nixpkgs.pkgsi686Linux.mesa.drivers;
   };
 }
