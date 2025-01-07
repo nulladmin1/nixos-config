8d96d0e51a1acbae165d5593b39c1b148d2c14b7
nulladmin1 <shrey.deogade@protonmail.com>
2025-01-06 21:43:43 -0500

    Switch ghostty to use home-manager module. still can't figure out keybindings

--- home/ghostty.nix
+++ home/ghostty.nix
@@ -1,42 +1,37 @@
 {
   pkgs,
-  lib,
   inputs,
   config,
   ...
-}: let
-  configFile = config.xdg.configHome + "/ghostty/config";
-  inherit (lib.strings) optionalString;
-in {
-  home.packages = with inputs; [
-    ghostty.packages.${pkgs.system}.default
-  ];
+}: {
   # TODO - figure out ghostty keybinding syntax
-  home.file.${configFile}.text =
-    ''
-      theme = catppuccin-${config.catppuccin.flavor}
-      font-family = ${config.stylix.fonts.monospace.name}
-      background-opacity = ${builtins.toString config.stylix.opacity.terminal}
-      window-theme = system
-    ''
-    + optionalString (!config.programs.zellij.enableBashIntegration) ''
+  programs.ghostty = {
+    enable = true;
+    package = inputs.ghostty.packages.${pkgs.system}.default;
 
-      # Zellij-compliant keybinds
+    settings = {
+      theme = "catppuccin-${config.catppuccin.flavor}";
+      font-family = config.stylix.fonts.monospace.name;
+      background-opacity = builtins.toString config.stylix.opacity.terminal;
 
-      # Split related stuff
-      keybind = ctrl+p>n=new_split
+      window-theme = "system";
 
-      # Split navigation
-      keybind = ctrl+p>up=goto_split:top
-      keybind = ctrl+p>down=goto_split:bottom
-      keybind = ctrl+p>left=goto_split:left
-      keybind = ctrl+p>right=goto_split:right
+      # Keybinds
+      #keybind = [
+      #  "ctrl+p>n=new_split"
+      #
+      #  "ctrl+p>up=goto_split:top"
+      #  "ctrl+p>down=goto_split:bottom"
+      #  "ctrl+p>left=goto_split:left"
+      #  "ctrl+p>right=goto_split:right"
 
-      # Tab-related stuff
-      keybind = ctrl+t>n=new_tab
+      #  "ctrl+t>n=new_tab"
 
-      # Window-related stuff (inherits same leader key as Tab)
-      keybind = ctrl+t>w=new_window
-      keybind = ctrl+t>o=toggle_tab_overview
-    '';
+      #  "ctrl+t>w=new_window"
+      #  "ctrl+t>o=toggle_tab_overview"
+      #];
+    };
+
+    clearDefaultKeybinds = true;
+  };
 }
