8976ad51043e0424c8534400186fdc0da37c0d04
nulladmin1 <shrey.deogade@protonmail.com>
2025-01-12 15:22:21 -0500

    Make dealing with Nix caches overcomplicated

--- system/nix.nix
+++ system/nix.nix
@@ -1,21 +1,30 @@
-{...}: {
-  nix = {
+{lib, ...}: {
+  nix = let
+    cachixSubstituters = caches: map (cache: "https://${cache}.cachix.org") caches;
+    cachixKeys = caches: keys: lib.lists.zipListsWith (cache: key: "${cache}.cachix.org-1:${key}") caches keys;
+  in {
     channel.enable = false;
     settings = {
       auto-optimise-store = true;
       experimental-features = ["nix-command" "flakes"];
-      substituters = [
-        "https://hyprland.cachix.org"
-        "https://ghostty.cachix.org"
-        "https://nix-community.cachix.org"
-        "https://cuda-maintainers.cachix.org"
+      substituters = cachixSubstituters [
+        "hyprland"
+        "ghostty"
+        "nix-community"
+        "cuda-maintainers"
       ];
-      trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      ];
+      trusted-public-keys =
+        cachixKeys [
+          "hyprland"
+          "ghostty"
+          "nix-community"
+          "cuda-maintainers"
+        ] [
+          "a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
+          "mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        ];
     };
   };
 
