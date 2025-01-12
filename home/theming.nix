d09b3de5569deac154111e3f5d126568d25e3587
nulladmin1 <shrey.deogade@protonmail.com>
2025-01-12 12:26:07 -0500

    Declaratively install theme for ghostwriter

--- home/theming.nix
+++ home/theming.nix
@@ -1,6 +1,9 @@
 {
   osConfig,
+  config,
   inputs,
+  pkgs,
+  lib,
   ...
 }: let
   inherit (osConfig.catppuccin) enable flavor accent;
@@ -26,4 +29,19 @@ in {
   catppuccin = {
     inherit enable flavor accent;
   };
+
+  # Ghostwriter
+  xdg.dataFile = let
+    themeRepo = pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "ghostwriter";
+      rev = "183823f462b2a8feccdcc2a5f0ab5a86ac9fd985";
+      hash = "sha256-7ptq1Ix0ebqWSUZ6u2mK9UkA4nm/B7R7aK835tr8Wo0=";
+    };
+
+    themeFile = "catppuccin-${flavor}-${accent}.json";
+  in
+    lib.mkIf (builtins.elem pkgs.ghostwriter config.home.packages) {
+      "ghostwriter/themes/${themeFile}".source = "${themeRepo}/themes/${themeFile}";
+    };
 }
