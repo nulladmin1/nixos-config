8fca22d12fd6ae2f9585e4c6d73980f516b51189
nulladmin1 <shrey.deogade@protonmail.com>
2025-03-07 21:31:57 -0500

    Add Plasma-manager config

--- home/desktop/plasma.nix
+++ home/desktop/plasma.nix
@@ -1,5 +1,65 @@
-{inputs, ...}: {
+{
+  inputs,
+  config,
+  osConfig,
+  ...
+}: {
   imports = [
     inputs.plasma-manager.homeManagerModules.plasma-manager
   ];
+
+  programs.plasma = let
+    inherit (config.var) wallpaper;
+  in {
+    inherit (osConfig.services.desktopManager.plasma6) enable;
+
+    workspace = {
+      inherit wallpaper;
+      lookAndFeel = "Catppuccin-Mocha-Mauve";
+      colorScheme = "CatppuccinMochaMauve";
+      iconTheme = config.stylix.iconTheme.dark;
+
+      cursor = {
+        inherit (config.stylix.cursor) size;
+        theme = config.stylix.cursor.name;
+      };
+    };
+
+    fonts = {
+      fixedWidth = {
+        family = config.stylix.fonts.monospace.name;
+        pointSize = 12;
+      };
+    };
+
+    hotkeys.commands = {
+      "Launch-Alacritty" = {
+        name = "Launch Alacritty";
+        command = "alacritty";
+        comment = "Launch Alacritty terminal emulator";
+        key = "Meta+Return";
+      };
+    };
+
+    krunner = {
+      shortcuts.launch = "Meta+R";
+    };
+
+    kscreenlocker = {
+      appearance = {
+        inherit wallpaper;
+      };
+    };
+
+    kwin = {
+      effects = {
+        blur.enable = true;
+        cube.enable = true;
+        fallApart.enable = true;
+        minimization.animation = "magiclamp";
+        translucency.enable = true;
+        wobblyWindows.enable = true;
+      };
+    };
+  };
 }
