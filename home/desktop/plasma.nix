69d9f1dab8ea89c895f8bfff6ca3a9258e149738
nulladmin1 <shrey.deogade@protonmail.com>
2025-03-07 23:23:32 -0500

    Add shortcuts to Plasma Manager

--- home/desktop/plasma.nix
+++ home/desktop/plasma.nix
@@ -41,6 +41,36 @@
       };
     };
 
+    shortcuts = {
+      KWin = {
+        "Switch to Desktop 1" = "Meta+1";
+        "Switch to Desktop 2" = "Meta+2";
+        "Switch to Desktop 3" = "Meta+3";
+        "Switch to Desktop 4" = "Meta+4";
+        "Switch to Desktop 5" = "Meta+5";
+        "Switch to Desktop 6" = "Meta+6";
+        "Switch to Desktop 7" = "Meta+7";
+        "Switch to Desktop 8" = "Meta+8";
+        "Switch to Desktop 9" = "Meta+9";
+        "Switch to Desktop 10" = "Meta+0";
+
+        "Window to Desktop 1" = "Meta+Shift+1";
+        "Window to Desktop 2" = "Meta+Shift+2";
+        "Window to Desktop 3" = "Meta+Shift+3";
+        "Window to Desktop 4" = "Meta+Shift+4";
+        "Window to Desktop 5" = "Meta+Shift+5";
+        "Window to Desktop 6" = "Meta+Shift+6";
+        "Window to Desktop 7" = "Meta+Shift+7";
+        "Window to Desktop 8" = "Meta+Shift+8";
+        "Window to Desktop 9" = "Meta+Shift+9";
+        "Window to Desktop 10" = "Meta+Shift+0";
+
+        "Close Window" = "Meta+W";
+        "Make Window Fullscreen" = "Meta+F";
+        "Toggle Overview" = "Meta+Tab";
+      };
+    };
+
     krunner = {
       shortcuts.launch = "Meta+R";
       position = "center";
