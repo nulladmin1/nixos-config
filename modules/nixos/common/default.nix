2d0d0da555d47210bf4404d4862ad549ade1e9d2
nulladmin1 <shrey.deogade@protonmail.com>
2025-06-17 15:58:54 -0400

    Add common module

---
+++ modules/nixos/common/default.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  moduleName = "common";
+in {
+  options.custom.${moduleName} = {
+    enable = lib.options.mkEnableOption moduleName;
+  };
+
+  config = lib.mkIf config.custom.${moduleName}.enable {
+    custom = {
+      audio.enable = true;
+      fonts.enable = true;
+      vscode.enable = true;
+      gdm.enable = true;
+      gnome.enable = true;
+      hardware.enable = true;
+      hyprland.enable = true;
+      kdeconnect.enable = true;
+      locales.enable = true;
+      nh.enable = true;
+      nix.enable = true;
+      packages.enable = true;
+      printing.enable = true;
+      sops.enable = true;
+      theming.enable = true;
+      users.enable = true;
+      utils.enable = true;
+      virtualization.enable = true;
+      xdg-portal.enable = true;
+    };
+  };
+}
