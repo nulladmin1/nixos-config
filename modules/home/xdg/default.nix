83365b5bbee9ef73bf0a8743f54e7c9669eb2f96
nulladmin1 <shrey.deogade@protonmail.com>
2025-08-23 01:36:05 -0400

    Add XDG mime types

--- modules/home/xdg/default.nix
+++ modules/home/xdg/default.nix
@@ -24,12 +24,17 @@ in {
       mimeApps = {
         enable = true;
         defaultApplications = {
-          "application/xhtml+xml" = "brave-browser.desktop";
-          "text/html" = "brave-browse.desktop";
-          "text/xml" = "brave-browser.desktop";
-          "x-scheme-handler/ftp" = "brave-browser.desktop";
-          "x-scheme-handler/http" = "brave-browser.desktop";
-          "x-scheme-handler/https" = "brave-browser.desktop";
+          "application/xhtml+xml" = ["brave-browser.desktop"];
+          "text/html" = ["brave-browse.desktop"];
+          "text/xml" = ["brave-browser.desktop"];
+          "x-scheme-handler/ftp" = ["brave-browser.desktop"];
+          "x-scheme-handler/http" = ["brave-browser.desktop"];
+          "x-scheme-handler/https" = ["brave-browser.desktop"];
+          "application/pdf" = ["okular.desktop" "brave.desktop"];
+          "application/zip" = ["nemo.desktop"];
+          "image/*" = ["feh.desktop"];
+          "text/*" = ["code.desktop"];
+          "video/*" = ["vlc.desktop"];
         };
       };
     };
