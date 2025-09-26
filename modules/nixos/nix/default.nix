141a366772da57f7e4a6e11fe950434d83f7a3b3
nulladmin1 <shrey.deogade@protonmail.com>
2025-09-26 17:46:01 -0400

    Simplify substituter cahce config

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -10,29 +10,26 @@ in {
     enable = lib.options.mkEnableOption moduleName;
   };
 
-  config = let
-    cachixSubstituters = caches: map (cache: "https://${cache}.cachix.org") caches;
-    cachixKeys = caches: keys: lib.lists.zipListsWith (cache: key: "${cache}.cachix.org-1:${key}") caches keys;
-  in
-    lib.mkIf config.custom.${moduleName}.enable {
-      nix = {
-        nixPath = ["nixpkgs=${inputs.nixpkgs}"];
-        settings = let
-          substituters = [
-            "hyprland"
-            "nix-community"
-            "cuda-maintainers"
-          ];
-        in {
-          auto-optimise-store = true;
-          experimental-features = ["nix-command" "flakes"];
-          substituters = cachixSubstituters substituters;
-          trusted-public-keys = cachixKeys substituters [
-            "a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-            "mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-            "0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-          ];
-        };
+  config = lib.mkIf config.custom.${moduleName}.enable {
+    nix = {
+      nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+      settings = let
+      in {
+        auto-optimise-store = true;
+        experimental-features = ["nix-command" "flakes"];
+        substituters = [
+          "https://hyprland.cachix.org"
+          "https://nix-community.cachix.org"
+          "https://cuda-maintainers.cachix.org"
+          "https://vicinae.cachix.org"
+        ];
+        trusted-public-keys = [
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+          "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
+        ];
       };
     };
+  };
 }
