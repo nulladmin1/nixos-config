d0033d7494baabdb298daa8e2b6bc4e249b21b98
nulladmin1 <shrey.deogade@protonmail.com>
2025-09-26 17:45:45 -0400

    Correctly enable gnome-keyring

--- modules/nixos/keyring/default.nix
+++ modules/nixos/keyring/default.nix
@@ -9,9 +9,7 @@
 
   config = lib.mkIf config.custom.gnome-keyring.enable {
     services.gnome.gnome-keyring.enable = true;
-    security.pam.services = {
-      greetd.enableGnomeKeyring = true;
-    };
+    security.pam.services.greetd.enableGnomeKeyring = true;
     programs.seahorse.enable = true;
   };
 }
